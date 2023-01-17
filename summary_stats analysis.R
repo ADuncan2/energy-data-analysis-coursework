2958aa152a0bfe86432b40019f8e2927c70a699e
ADuncan2 <55701708+ADuncan2@users.noreply.github.com>
2023-01-17 14:59:02 +0000

    Summary stats visuals added

---
+++ summary_stats analysis.R
@@ -0,0 +1,17 @@
+library(dplyr)
+library(tictoc)
+library(ggplot2)
+library(ggdist)
+library(tidyr)
+library(plotly)
+library(data.table)
+
+setwd("~/Energy data analysis coursework/energy-data-analysis-coursework")
+
+data <- read.csv("summary_stats.csv")
+
+data%>%
+  select(-start,-end,-obs)%>%
+  pivot_longer(!LCLid, names_to = "names",values_to = "values")%>%
+  group_by(names)%>%
+  ggplot(aes(y=names,x=values))+
