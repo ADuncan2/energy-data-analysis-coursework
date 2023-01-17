cd82795a51172f5a6e1c76de7d877fa8d8aefcca
ADuncan2 <55701708+ADuncan2@users.noreply.github.com>
2023-01-17 17:30:39 +0000

    max load for multiple batches
    Max load csv added and first graph of 7 batches saved

--- peak loads variation.R
+++ peak loads variation.R
@@ -8,41 +8,53 @@ library(data.table)
 
 setwd("~/Energy data analysis coursework/energy-data-analysis-coursework")
 
+for (j in 1:2){
+  x <- "~/Energy data analysis coursework/data/LCL-June2015v2_"
+  num <- as.character(j)
+  name<- paste(x,num,".csv",sep="")
+  data <- read.csv(name)
 
-
-data <- read.csv("~/Energy data analysis coursework/data/LCL-June2015v2_3.csv")
-
-data1<- data %>%
-  rename(KWh = KWH.hh..per.half.hour.)%>%
-  mutate(KWh1 = as.numeric(as.character(KWh)),
-         datetime = as.POSIXct(DateTime,"%Y-%m-%d %H:%M:%S",tz="GMT"),
-         time = format(datetime,"%H:%M:%S"),
-         month = months.POSIXt(datetime),
-         date = format(datetime,"%y-%m-%d"))%>%
-  group_by(LCLid)
-
-
-
-max_id <- max(data1$LCLid)
-min_id <- min(data1$LCLid)
-
-n = max_id - min_id
-
-datalist = vector("list", length = n)
-
-for(i in min_id:max_id){
-data2<- data1%>%
-  filter(LCLid %in% min_id:i)%>%
-  group_by(date,time)%>%
-  summarise(kWh_sum= sum(KWh1))%>%
-  filter(!is.na(kWh_sum))%>%
-  summarise(number = i-min_id,max = max(kWh_sum),max_norm = max/number)
-datalist[[i+1-min_id]]<-data2
+  data1<- data %>%
+    rename(KWh = KWH.hh..per.half.hour.)%>%
+    mutate(KWh1 = as.numeric(as.character(KWh)),
+           datetime = as.POSIXct(DateTime,"%Y-%m-%d %H:%M:%S",tz="GMT"),
+           time = format(datetime,"%H:%M:%S"),
+           month = months.POSIXt(datetime),
+           date = format(datetime,"%y-%m-%d"))%>%
+    group_by(LCLid)
+  
+  
+  
+  max_id <- max(data1$LCLid)
+  min_id <- min(data1$LCLid)
+  
+  n = max_id - min_id
+  
+  datalist = vector("list", length = n)
+  
+  for(i in min_id:max_id){
+  data2<- data1%>%
+    filter(LCLid %in% min_id:i)%>%
+    group_by(date,time)%>%
+    summarise(kWh_sum= sum(KWh1))%>%
+    filter(!is.na(kWh_sum))%>%
+    summarise(number = i+1-min_id,max = max(kWh_sum),max_norm = max/number)
+  datalist[[i+1-min_id]]<-data2
+  }
+  
+  big_data = do.call(rbind, datalist)
+  
+  big_data%>%
+    group_by(number)%>%
+    summarise(max = max(max_norm))
+  
+  fwrite(big_data, file = "max_load.csv", sep = ",",
+         append = TRUE)
 }
 
-big_data = do.call(rbind, datalist)
+max_load <- read.csv("max_load.csv")
 
-big_data%>%
+max_load%>%
   group_by(number)%>%
   summarise(max = max(max_norm))%>%
   ggplot(aes(number,max))+
