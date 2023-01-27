d93ad7ba23b4c783fefe1ea611b5ada2df48c141
ADuncan2 <55701708+ADuncan2@users.noreply.github.com>
2023-01-27 16:10:41 +0000

    weekly metrics generator created
    new version of peak load var created with weekly metric function

--- peak loads variation v3.R
+++ peak loads variation v3.R
@@ -9,6 +9,7 @@ library(arrow)
 library(readr)
 library(gganimate)
 library(ggridges)
+library(lubridate)
 
 setwd("~/Energy data analysis coursework/energy-data-analysis-coursework")
 
@@ -173,117 +174,77 @@ multi_runs_cont2%>%
   filter(house%%2==0)%>%
   mutate(house = as.factor(house))%>%
   ggplot(aes(x = values, y = house)) +
-    geom_density_ridges(scale = 4) +
-    scale_y_discrete(expand = c(0, 0)) +
-    scale_x_continuous(expand = c(0, 0)) +
-    coord_cartesian(clip = "off") +
-    theme_ridges(grid = F) +
-    guides(fill = F)+
+  geom_density_ridges(scale = 4) +
+  scale_y_discrete(expand = c(0, 0)) +
+  scale_x_continuous(expand = c(0, 0)) +
+  coord_cartesian(clip = "off") +
+  theme_ridges(grid = F) +
+  guides(fill = F)+
   labs(x="Demand diversity factor",y="Number house houses in sample",
        title = "Density of demand diveristy factor for 1000 runs of 20 randomly sampled control homes")
 
 
+#### weekly metric calculation
 
 
 
-####older code####
-admd<- data.frame()
-
-tic()
-for (i in 1:num_of_homes){
-  data_max_tot <- data[,1:(i+1)]%>%
+run_weekly_test <- function(num){
+  #finding sample of data and max kwh in study period
+  num_of_homes <-  num
+  num_cols <- ncol(data_t)
+  ran_sample <- sample(3:num_cols,num_of_homes,replace = FALSE)
+  
+  data_time <- data_t[,2]
+  data_sample <- data_t[,ran_sample]
+  colnames(data_sample)<-as.numeric(1:num_of_homes)
+  data <- cbind(data_time,data_sample)
+  
+  data[is.na(data)]<-0
+  
+  data_max_ind <- data%>%
     pivot_longer(!DateTime,names_to = "house",values_to = "kwh")%>%
-    group_by(house)%>%
-    filter(!is.na(kwh))%>%
-    summarise(max_kwh=max(kwh))%>%
-    ungroup()%>%
-    summarise(max_indi_kwh = sum(max_kwh))
-
-  data_div_tot <- data[,1:(i+1)]%>%
+    mutate(date = floor_date(DateTime,unit = "week"))%>%
+    group_by(date)%>%
+    summarise(max_kwh = max(kwh))
+  
+  data_max_cum <- data%>%
     pivot_longer(!DateTime,names_to = "house",values_to = "kwh")%>%
     group_by(DateTime)%>%
-    filter(!is.na(kwh))%>%
-    summarise(sum_kwh=sum(kwh))%>%
+    summarise(sum_kwh = sum(kwh))%>%
+    mutate(date = floor_date(DateTime,unit = "week"))%>%
+    group_by(date)%>%
+    summarise(max_cum = max(sum_kwh))%>%
+    right_join(data_max_ind,by="date")%>%
+    mutate(admd=max_kwh/max_cum, coinc = 1/admd)%>%
     ungroup()%>%
-    summarise(div_max_kwh = max(sum_kwh))
-  admd[i,1]<-i
-  admd[i,2]<-data_max_tot$max_indi_kwh/data_div_tot$div_max_kwh
+    select(admd,coinc)
+  
+  return(data_max_cum)
+}
+
+tic()
+num_of_props<- 20 
+num_of_runs <- 10
+multi_runs_week <- data_t[,2]%>%
+  mutate(date = floor_date(DateTime,unit = "week"))%>%
+  select(-DateTime)%>%
+  distinct()
+
+#clearing old data
+rm(results)
+
+for (i in 1:num_of_runs){
+  results <- run_weekly_test(num_of_props)
+  coinc_name <- paste("coinc",i)
+  admd_name <- paste("admd",i)
+  colnames(results)<- c(admd_name,coinc_name)
+  multi_runs_week<- cbind(multi_runs_week,results)
 }
 toc()
-admd<- admd%>%
-  rename("index"=V1,"admd"=V2)%>%
-  mutate(coinc = 1/admd)
 
+multi_runs_week2<- multi_runs_week%>%
+  pivot_longer(!date)%>%
+  filter(grepl("admd",name))
 
-plot(x,y, type = "l", lwd = 2, axes = FALSE, xlab = "", ylab = "")
-axis(1, at = -3:3, labels = c("-3s", "-2s", "-1s", "mean", "1s", "2s", "3s"))
-
-plot <- ggplot(admd,aes(x=index,y=coinc))+
-  geom_point()+
-  labs(x="Number of properties (randomly selected)",
-       y="After diversity maximum demand")
-
-ggplotly(plot)
-
-#investigating a single homes
-#home ID
-investigate <- 22
-
-data_sample1 <- data_c[,ran_sample[investigate]]
-data1 <- cbind(data_time,data_sample1)
-
-inv<- data1%>%
-  pivot_longer(!DateTime,names_to = "house",values_to = "kwh")%>%
-  ggplot(aes(DateTime,kwh,color = house))+
+ggplot(multi_runs_week2,aes(x=date,y=value,color = name))+
   geom_point()
-
-ggplotly(inv)
-
-#distribution of average half hourly measurements in control group
-avg_hh_kwh<- data.frame()
-
-sample_of_homes <- 1000
-tic()
-for (i in 1:sample_of_homes){
-  data_avg <- data_c[,i+1]
-  data_tot<-cbind(data_time,data_avg)
-  name <- colnames(data_tot)[2]
-  
-  colnames(data_tot)<-c("time","home")
-  
-  data_avg_tot <- data_tot %>%
-    filter(!is.na(home))%>%
-    summarise(sum_kwh = sum(home),n = n(),avg_kwh = sum_kwh/n)
-  
-  avg_hh_kwh[i,1]<-name
-  avg_hh_kwh[i,2]<- data_avg_tot$avg_kwh
-}
-toc()
-
-ggplot(avg_hh_kwh,aes(x=V2))+
-  geom_histogram(bins = 50)
-
-
-#distribution of average half hourly measurements in control group
-max_hh_kwh<- data.frame()
-
-sample_of_homes <- 4445
-tic()
-for (i in 1:sample_of_homes){
-  data_max <- data_c[,i+1]
-  data_tot_max<-cbind(data_time,data_max)
-  name <- colnames(data_tot)[2]
-  
-  colnames(data_tot_max)<-c("time","home")
-  
-  data_avg_tot <- data_tot_max %>%
-    filter(!is.na(home))%>%
-    summarise(max_kwh = max(home))
-  
-  max_hh_kwh[i,1]<-name
-  max_hh_kwh[i,2]<- data_avg_tot$max_kwh
-}
-toc()
-
-ggplot(max_hh_kwh,aes(x=V2))+
-  geom_histogram(bins = 50)
