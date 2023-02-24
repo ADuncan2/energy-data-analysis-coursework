474cda606883307eaf87da6fdab0020ef02c4ff0
ADuncan2 <55701708+ADuncan2@users.noreply.github.com>
2023-02-24 12:57:00 +0000

    Working monthly metric calculation
    Moving on to weekly

--- peak loads variation v4.R
+++ peak loads variation v4.R
@@ -3,6 +3,7 @@ library(tictoc)
 library(ggplot2)
 library(ggdist)
 library(tidyr)
+library(tidyverse)
 library(plotly)
 library(data.table)
 library(arrow)
@@ -25,7 +26,7 @@ data_t <- data_t%>%
 reps <- 500
 num_of_homes <- 100
 
-metrics_calc <- function(num_of_homes){
+data_generation <- function(num_of_homes){
   num <- num_of_homes
   num_cols <- ncol(data_t)
   ran_sample <- sample(2:num_cols,num_of_homes,replace = FALSE)
@@ -35,22 +36,24 @@ metrics_calc <- function(num_of_homes){
   data[is.na(data)]<-0 #note this, it's potentially a big assumption
   
   ## partition based on period
-  period <- "month"
-  
+  return(data)}
+
+metrics_calc <- function(data,period){
   data_period <- data%>%
     mutate(period = floor_date(DateTime,unit = period))%>%
     group_split(period)
+    
+  test_list<-lapply(data_period,coinc_admd_calc)
   
-  
-  
-  test<-lapply(data_period,coinc_admd_calc)
-  
-  return(coinc)
+  return(test_list)
 }
 
 
 coinc_admd_calc <- function(data){
-  data_names <- as.data.frame(colnames(data)[2:101])%>%
+  data <- data%>%
+    dplyr::select(-period)
+  
+  data_names <- as.data.frame(colnames(data)[2:(num_of_homes+1)])%>%
     mutate(house = 1:num_of_homes)
   
   #changing names to numbers to ensure order of houses is kept consistent when cumuliative sum is applied
@@ -75,8 +78,45 @@ coinc_admd_calc <- function(data){
     summarise(max_cum = max(cumsum))%>%
     right_join(data_max_ind,by="house")%>%
     mutate(admd=max_indi/max_cum, coinc = 1/admd,div_max = max_cum/house)%>%
-    dplyr::select(admd,coinc,div_max)
+    dplyr::select(admd,coinc,div_max)%>%
+    mutate(house = 1:num_of_homes)%>%
+    dplyr::arrange(house,admd,coinc,div_max)%>%
+    pivot_longer(!house)
     
   return(data_max_cum)
 }
 
+num_of_homes<-100
+
+data_sample <- data_generation(num_of_homes)
+
+period <- "month"
+
+data_period <- data_sample%>%
+  mutate(period = floor_date(DateTime,unit = period))%>%
+  dplyr::select(period)%>%
+  summarise(period = unique(period))
+
+metrics <- metrics_calc(data_sample,"month")
+
+##adding periods back into each list of metrics
+for (i in 1:nrow(data_period)){
+  metrics[[i]]<- metrics[[i]]%>%
+    mutate(date = data_period$period[i])
+}
+
+metrics_joined <- metrics%>%
+  rbindlist()
+
+###for testing
+metrics_test <- metrics[[10]]
+
+metrics_test<- metrics_test%>%
+  pivot_longer(!house)%>%
+  mutate(date=data_period$period[10])
+  
+
+metrics_joined%>%
+  filter(name=="div_max")%>%
+  ggplot(aes(x=date,y=value,colour = house))+
+  geom_point()
