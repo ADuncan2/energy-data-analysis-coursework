f5e9be4650a82823b0c38d0c4aec9d099a49ae34
ADuncan2 <55701708+ADuncan2@users.noreply.github.com>
2023-03-06 14:27:33 +0000

    Repeat function for parametric fit added

--- peak loads variation v4.R
+++ peak loads variation v4.R
@@ -18,8 +18,55 @@ setwd("~/Energy data analysis coursework/energy-data-analysis-coursework")
 data_c <- read_csv("~/Energy data analysis coursework/data/Matrix/MatrixC.csv")
 data_t <- read_csv("~/Energy data analysis coursework/data/Matrix/MatrixT.csv")
 
+
+
 data_t <- data_t%>%
   dplyr::select(-...1)
+
+### slimmed down version of functions for quicker repeats
+
+rep_for_param <- function(data_type_rep,num_homes_rep){
+  data_rep <- data_generation(num_homes_rep,data_type_rep)
+  
+  data_rep1 <- data_rep%>%
+    pivot_longer(!DateTime,names_to = "house",values_to = "kwh")
+  
+  data_max_ind <- data_rep1%>%
+    group_by(house)%>%
+    summarise(max_kwh = max(kwh))%>%
+    summarise(sum_max_kwh = sum(max_kwh))
+  
+  data_max_cum <- data_rep1%>%
+    group_by(DateTime)%>%
+    summarise(sum_kwh = sum(kwh))%>%
+    summarise(max_sum_kwh = max(sum_kwh))%>%
+    bind_cols(data_max_ind)%>%
+    mutate(coinc = max_sum_kwh/sum_max_kwh,
+           admd = 1/coinc,
+           div_max = max_sum_kwh/num_homes_rep)%>%
+    dplyr::select(coinc,admd,div_max)
+  return(data_max_cum)
+}
+
+num_homes_rep <- 20
+data_type_rep <- "control"
+
+tic("overall")
+for (i in 1:10){
+  data <- rep_for_param(data_type_rep,num_homes_rep)
+
+  if(i==1){
+    data_store_rep <- data
+  }
+  else{
+    data_store_rep <- rbind(data_store_rep,data)
+  }
+}
+toc()
+
+
+
+
 
 ### setting the sample size
 
@@ -50,7 +97,7 @@ metrics_calc <- function(data,period){
   data_period <- data%>%
     mutate(period = floor_date(DateTime,unit = period))%>%
     group_split(period)
-    
+  
   test_list<-lapply(data_period,coinc_admd_calc)
   
   return(test_list)
@@ -66,7 +113,7 @@ coinc_admd_calc <- function(data){
   
   #changing names to numbers to ensure order of houses is kept consistent when cumuliative sum is applied
   colnames(data)<- c("DateTime",data_names$house)
-  
+
   data_max_ind <- data%>%
     pivot_longer(!DateTime,names_to = "house",values_to = "kwh")%>%
     mutate(house = as.numeric(house))%>%
@@ -74,6 +121,7 @@ coinc_admd_calc <- function(data){
     summarise(max_kwh = max(kwh))%>%
     mutate(max_indi = cumsum(max_kwh))%>%
     dplyr::select(-max_kwh)
+
   
   data_max_cum <- data%>%
     pivot_longer(!DateTime,names_to = "house",values_to = "kwh")%>%
@@ -90,28 +138,32 @@ coinc_admd_calc <- function(data){
     mutate(house = 1:num_of_homes)%>%
     dplyr::arrange(house,admd,coinc,div_max)%>%
     pivot_longer(!house)%>%
-    filter(house %in% list(1,3,10,50,100,200))
-    
+    filter(house %in% list(1,3,10,50,100,150))
+  
   return(data_max_cum)
 }
 
 
 ###variables ###
-num_of_homes<-100
+num_of_homes<-200
 data_type <- "control"
-period <- "season"
+period <- "month"
 
 
 
 repeat_calc <- function(num_of_homes,data_type,period){
+  tic("sample gen")
   data_sample <- data_generation(num_of_homes,data_type)
+  toc()
   
   data_period <- data_sample%>%
     mutate(period = floor_date(DateTime,unit = period))%>%
     dplyr::select(period)%>%
     summarise(period = unique(period))
   
+  tic("metrics")
   metrics <- metrics_calc(data_sample,period)
+  toc()
   
   ##adding periods back into each list of metrics
   for (i in 1:nrow(data_period)){
@@ -124,10 +176,12 @@ repeat_calc <- function(num_of_homes,data_type,period){
   return(metrics_joined)
 }
 
+tic("overall")
 test_data <- repeat_calc(num_of_homes,data_type,period)
+toc()
 
-tic()
-for (i in 1:30){
+tic("overall")
+for (i in 1:40){
   data <- repeat_calc(num_of_homes,data_type,period)
   if(i==1){
     data_store <- data
@@ -149,18 +203,8 @@ data_store%>%
 
 
 data_store%>%
-  filter(name=="div_max",house == 100,date>as.POSIXct("2013-01-01"))%>%
+  filter(name=="div_max",house == 100,date==as.POSIXct("2013-12-01"))%>%
   mutate(date = as.factor(date))%>%
   ggplot(aes(x=value, fill = date))+
-  geom_density()
+  geom_histogram(bins=5)
   
-
-metrics_joined%>%
-  filter(name=="admd",house>30)%>%
-  ggplot(aes(x=date,y=value,colour = house))+
-  geom_point()
-
-metrics_joined_c%>%
-  filter(name=="div_max",house>30,date>as.POSIXct("2013-01-01"))%>%
-  ggplot(aes(x=date,y=value,colour = house))+
-  geom_point()
